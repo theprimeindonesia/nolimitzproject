c9c5b0f1c233e097ddf31e18af5378b70c373b98
Adzhar Amrullah <adzharamrullah@gmail.com>
2018-08-30 09:45:02 +0700

    test welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Bradah
                 </div>
 
                 <div class="links">
